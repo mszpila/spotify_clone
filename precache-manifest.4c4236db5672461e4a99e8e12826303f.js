ee760db099f43f71ce3a19d3d9f93bfd9e19ffa9
mszpila <szpila.mikolaj@gmail.com>
2020-08-26 10:28:10 +0200

    Updates

---
+++ precache-manifest.4c4236db5672461e4a99e8e12826303f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37490e947b67b78b83dba4f8d3f642a5",
+    "url": "/spotify_clone/index.html"
+  },
+  {
+    "revision": "399e36e035265b6d1ce4",
+    "url": "/spotify_clone/static/css/main.3b413b8a.chunk.css"
+  },
+  {
+    "revision": "01ec64b9e73b87b3d082",
+    "url": "/spotify_clone/static/js/2.0b96e058.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/spotify_clone/static/js/2.0b96e058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "399e36e035265b6d1ce4",
+    "url": "/spotify_clone/static/js/main.08ac853d.chunk.js"
+  },
+  {
+    "revision": "8cebd115bf194f0476fe",
+    "url": "/spotify_clone/static/js/runtime-main.7f1af19c.js"
+  }
+]);
